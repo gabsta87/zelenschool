2008c2020f0fd25c315568f4ff89b9bb1738b3d0
Gabriel Maret <principal@MBP1.local>
2022-12-03 13:37:35 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './features/coursesmanager/components/homepage/homepage.component';
+import { LoginpageComponent } from './features/coursesmanager/components/loginpage/loginpage.component';
+import { HeaderComponent } from './features/coursesmanager/components/header/header.component';
+import { FooterComponent } from './features/coursesmanager/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginpageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
