6cb71def2b1e52a807577a7dbf61d9e5a567b80e
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-01-27 22:26:22 +0100

    student guard done

--- src/app/features/coursesmanager/guards/studentpage.guard.ts
+++ src/app/features/coursesmanager/guards/studentpage.guard.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Auth } from 'firebase/auth';
+import { AngularfireService } from 'src/app/shared/service/angularfire.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StudentpageGuard implements CanActivate, CanActivateChild {
-  canActivate(
+export class StudentpageGuard implements CanActivate {
+  constructor(private readonly _auth:Auth, private readonly _db:AngularfireService){}
+  async canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  canActivateChild(
-    childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): Promise<any> {
+
+      let userId = this._auth?.currentUser?.uid;
+
+      if(userId){
+        let userData = await this._db.getUser(userId);
+
+        if(userData)
+          return userData['status'] == "student";
+      }
+
+      return false
   }
   
 }
