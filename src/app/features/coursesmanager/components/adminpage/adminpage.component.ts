4c87a05141a87033067eeacffcfb8ca7c30a58dd
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-02-08 22:41:05 +0100

    removed useless variables from adminpage

--- src/app/features/coursesmanager/components/adminpage/adminpage.component.ts
+++ src/app/features/coursesmanager/components/adminpage/adminpage.component.ts
@@ -15,17 +15,12 @@ export class AdminpageComponent {
   ) {}
 
   adminData!:any;
-  usersList!:any;
   usersListObs!:Observable<any[]>;
   coursesObs!:Observable<any[]>;
-  coursesList!:any;
 
   ionViewWillEnter():void{
     this.adminData = this._route.snapshot.data['adminData'];
-    console.log("admin data : ",this.adminData);
-    this.usersList = this.adminData.users;
-    this.coursesList = this.adminData.courses;
-    // console.log("users list : ",this.usersList);
+    
     this.usersListObs = this.adminData.usersObs;
     this.coursesObs = this.adminData.coursesObs;
   }
