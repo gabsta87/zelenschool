7606fe707fe189c9cbf1becf1127acde440094c3
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-03-09 13:01:12 +0100

    choice modal compiles, need testing

--- src/app/features/coursesmanager/components/choice-modal/choice-modal.component.ts
+++ src/app/features/coursesmanager/components/choice-modal/choice-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-choice-modal',
@@ -6,13 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./choice-modal.component.scss']
 })
 export class ChoiceModalComponent {
+  constructor(private modalCtrl: ModalController) { }
 
   confirmStudent(){
-
+    return this.modalCtrl.dismiss(null, 'student');
   }
-
+  
   confirmTeacher(){
-    
+    return this.modalCtrl.dismiss(null, 'teacher');
   }
 
 }
