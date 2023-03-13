77f627c4107856a04608c34e9033eaaf1300b46e
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-03-13 14:31:14 +0100

    partners fixed

--- src/app/features/coursesmanager/components/partnerspage/partnerspage.component.ts
+++ src/app/features/coursesmanager/components/partnerspage/partnerspage.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./partnerspage.component.scss']
 })
 export class PartnerspageComponent {
+  partners = [
+    { logoName:"ukrainiandiaspora.png",link:"https://ukrainian-diaspora-geneva.ch/"},
+    { logoName:"Kultura-logo.jpeg",link:"http://kultura.ch/article-4-2/"},
+    { logoName:"deti.png",link:"https://detinow.ch/"},
+    ]
 
 }
