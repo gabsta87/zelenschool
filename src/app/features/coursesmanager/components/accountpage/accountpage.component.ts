0e2c06d62bc97ead9468fa12cfe5bedab3c57b79
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-03-22 11:22:52 +0100

    simplified accountpage

--- src/app/features/coursesmanager/components/accountpage/accountpage.component.ts
+++ src/app/features/coursesmanager/components/accountpage/accountpage.component.ts
@@ -14,33 +14,21 @@ export class AccountpageComponent {
   userData = this._route.snapshot.data['userData'];
   userObs!:Observable<any>;
 
-  profileForm!:FormGroup<any>;
+  profileForm = new FormGroup({
+    email : new FormControl(this.userData.user.email,emailValidator()),
+    f_name : new FormControl(this.userData.user.f_name,Validators.required),
+    l_name : new FormControl(this.userData.user.l_name,Validators.required),
+    phone : new FormControl(this.userData.user.phone,phoneValidator()),
+    address : new FormControl(this.userData.user.address,Validators.required),
+    s_permit_number : new FormControl(this.userData.user.s_permit_id,permitValidator()),
+    birthday : new FormControl(this.userData.user.birthday,bdValidator()),
+  });
 
   constructor(
     private readonly _user:UsermanagementService,
     private readonly _router:Router,
     private readonly _route:ActivatedRoute
-  ){
-    this.loadData()
-  }
-
-  ionViewWillEnter(){
-    this.loadData();
-  }
-
-  loadData(){
-    const myValue = this.userData.user;
-
-    this.profileForm = new FormGroup({
-      email : new FormControl(myValue.email,emailValidator()),
-      f_name : new FormControl(myValue.f_name,Validators.required),
-      l_name : new FormControl(myValue.l_name,Validators.required),
-      phone : new FormControl(myValue.phone,phoneValidator()),
-      address : new FormControl(myValue.address,Validators.required),
-      s_permit_number : new FormControl(myValue.s_permit_id,permitValidator()),
-      birthday : new FormControl(myValue.birthday,bdValidator()),
-    })
-  }
+  ){ }
 
   async logout(){
     this._user.logout();
@@ -48,7 +36,6 @@ export class AccountpageComponent {
   }
 
   update(){
-
     let updateItem = {
       email : this.profileForm.get('email')?.value,
       l_name : this.profileForm.get('l_name')?.value,
