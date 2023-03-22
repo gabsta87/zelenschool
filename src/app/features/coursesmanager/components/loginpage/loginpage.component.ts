5d6662f0682ef4934859ee0bf6749eb483bf492f
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-03-22 23:42:17 +0100

    started facebook login implementation

--- src/app/features/coursesmanager/components/loginpage/loginpage.component.ts
+++ src/app/features/coursesmanager/components/loginpage/loginpage.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth, getAuth, GoogleAuthProvider, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup } from '@angular/fire/auth';
+import { Auth, getAuth, GoogleAuthProvider, FacebookAuthProvider ,sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup } from '@angular/fire/auth';
 import { AngularfireService } from 'src/app/shared/service/angularfire.service';
 import { AlertController, ModalController } from '@ionic/angular';
 import { ChoiceModalComponent } from '../choice-modal/choice-modal.component';
@@ -12,12 +12,14 @@ import { ChoiceModalComponent } from '../choice-modal/choice-modal.component';
 })
 export class LoginpageComponent {
 
+
   email:string = "";
   password:string = "";
   error:string = "";
   loading!:boolean;
 
-  provider = new GoogleAuthProvider();
+  providerGoogle = new GoogleAuthProvider();
+  providerFB = new FacebookAuthProvider();
   auth = getAuth();
 
 
@@ -32,7 +34,16 @@ export class LoginpageComponent {
 
   async loginWithGoogle(){
     this.loading = true;
-    const credential = await signInWithPopup(this._auth,this.provider);
+    const credential = await signInWithPopup(this._auth,this.providerGoogle)
+    .catch(error => {
+      this.error = error.message
+      this.loading = false;
+    });
+
+    if(credential == null)
+      return;
+    
+    this.error = "";
     const createdUser = await this._dbAccess.createUser(credential.user);
 
     // User is new, and an entry was added to the database
@@ -58,6 +69,51 @@ export class LoginpageComponent {
     return credential;
   }
 
+  async loginWithFB() {
+    const url = "https://zelenschool-6981a.firebaseapp.com/__/auth/handler"
+    console.log("starting fb connexion");
+
+    this.providerFB = new FacebookAuthProvider();
+
+    // this.auth.signOut();
+    this.auth = getAuth();
+
+    signInWithPopup(this.auth, this.providerFB)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+      
+      console.log("logging in with FB");
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+
+      console.log("credential : ",credential);
+      
+      let accessToken = undefined;
+      if(credential)
+        accessToken = credential.accessToken;
+
+      console.log("access token : ",accessToken);
+      this.error = "";
+      
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
+      console.log("error while logging in with FB",error.message);
+      this.error = error.message;
+
+      // ...
+    });
+  }
+
   async loginWithEmail(){
     if(this.email == ""){
       this.error = "Please enter an email"
@@ -72,6 +128,7 @@ export class LoginpageComponent {
         this._dbAccess.createUser(user);
         this._router.navigate(['/schedule/']);
         this.loading = false;
+        this.error = "";
         return userCredential;
       })
       .catch((error) => {
