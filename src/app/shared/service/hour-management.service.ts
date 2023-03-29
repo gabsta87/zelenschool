ac8677fb00d8f9636508a026b8ab55c741db6076
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-03-29 18:08:38 +0200

    hour management service

--- src/app/shared/service/hour-management.service.ts
+++ src/app/shared/service/hour-management.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import * as dayjs from 'dayjs';
+import * as isBetween from 'dayjs/plugin/isBetween';
+import * as utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc)
+dayjs.extend(isBetween);
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +12,13 @@ import { Injectable } from '@angular/core';
 export class HourManagementService {
 
   constructor() { }
+
+  
+
+  private isColliding(startTime1:string, endTime1:string ,startTime2:string, endTime2:string){
+    if(dayjs(startTime1).isBetween(startTime2,endTime2,'minutes','[)')) return true;  // 1 starts in 2
+    if(dayjs(endTime1).isBetween(startTime2,endTime2,'minutes','(]')) return true;    // 1 ends in 2
+    if(dayjs(startTime2).isBetween(startTime1,endTime1,'minutes','[)'))return true;   // 2 starts in 1
+    return false;
+  }
 }
