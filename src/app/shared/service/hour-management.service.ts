f494ee761b56144b2ba24715a563f706b71dc33f
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-03-29 21:37:19 +0200

    hour manager improved

--- src/app/shared/service/hour-management.service.ts
+++ src/app/shared/service/hour-management.service.ts
@@ -9,16 +9,17 @@ dayjs.extend(isBetween);
 @Injectable({
   providedIn: 'root'
 })
-export class HourManagementService {
+export class HourManagementService { }
 
-  constructor() { }
-
-  
-
-  private isColliding(startTime1:string, endTime1:string ,startTime2:string, endTime2:string){
-    if(dayjs(startTime1).isBetween(startTime2,endTime2,'minutes','[)')) return true;  // 1 starts in 2
-    if(dayjs(endTime1).isBetween(startTime2,endTime2,'minutes','(]')) return true;    // 1 ends in 2
-    if(dayjs(startTime2).isBetween(startTime1,endTime1,'minutes','[)'))return true;   // 2 starts in 1
-    return false;
-  }
+export function formatTime(timeToFormat:string){
+  console.log("hour formatted : ",dayjs(timeToFormat).utc());
+  return dayjs(timeToFormat).utc();
 }
+
+export function isColliding(startTime1:string, endTime1:string ,startTime2:string, endTime2:string){
+  if(dayjs(startTime1).isBetween(startTime2,endTime2,'minutes','[)')) return true;  // 1 starts in 2
+  if(dayjs(endTime1).isBetween(startTime2,endTime2,'minutes','(]')) return true;    // 1 ends in 2
+  if(dayjs(startTime2).isBetween(startTime1,endTime1,'minutes','[)'))return true;   // 2 starts in 1
+  return false;
+}
+
