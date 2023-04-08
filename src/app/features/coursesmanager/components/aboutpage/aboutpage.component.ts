c5e2028efcf2418f6979d2bd035605140c8a9045
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-04-08 11:27:53 +0200

    nav-bar works correctly (without coloring aboutpage buttons)

--- src/app/features/coursesmanager/components/aboutpage/aboutpage.component.ts
+++ src/app/features/coursesmanager/components/aboutpage/aboutpage.component.ts
@@ -9,23 +9,18 @@ import { AngularfireService } from 'src/app/shared/service/angularfire.service';
   styleUrls: ['./aboutpage.component.scss']
 })
 export class AboutpageComponent {
-  @ViewChild('aboutIonContent') myDiv!: ElementRef;
   @ViewChild(IonContent)ionContent!: IonContent;
-  element!: HTMLElement;
+  @ViewChildren("anchor_") divs!:QueryList<ElementRef>;
+  @Output() selectButton : EventEmitter<number> = new EventEmitter();
 
-  constructor(private readonly _router : Router,private readonly _route: ActivatedRoute,private readonly _db: AngularfireService){
-    // TODO change subscribe to map
+  fragment!:any;
+  myAnchor!:any;
+
+  private _positions : any[] = [];
+  constructor(private readonly _route: ActivatedRoute){
+
     this._route.fragment.subscribe(fragment => { 
-      // console.log("updating fragment : ",fragment);
-      
-      this.fragment = fragment; 
-
-      this.myAnchor = document.querySelector('#' + this.fragment);
-      
-      // if(this.myAnchor && this.fragment){
-      //   console.log("subscribe : scrolling to");
-      //   this.scrollToElement(this.myAnchor);
-      // }
+      this.fragment = fragment;
     });
   }
 
@@ -33,58 +28,23 @@ export class AboutpageComponent {
   partners_addresses = this._route.snapshot.data['aboutData'].partners;
 
   ionViewWillEnter(){
-    console.log("Ion View Will Enter");
-    try {
-      if(document){
-        this.myAnchor = document.querySelector('#' + this.fragment);
-        console.log("anchor : ",this.myAnchor);
-        console.log("fragment : ",this.fragment);
-        
-        if(this.myAnchor && this.fragment){
-          console.log("ViewWillEnter : scrolling to");
-          
-          // this.scrollToElement(this.myAnchor);
-        }
-      }
-    } catch (e) { }
-  }
-
-  ngAfterViewInit(){
-    console.log("After view init");
-    if(this.myAnchor){
-      console.log("After view : scrolling to");
-      // this.scrollToElement(this.myAnchor);
-    }
+    if(this.fragment)
+      this.scrollTo(this.fragment);
   }
 
   ionViewDidEnter(){
-    console.log("Ion view did enter");
-    if(this.myAnchor){
-      console.log("DidEnter : scrolling to");
-      // this.scrollToElement(this.myAnchor);
-    }
+    this.initElemPositions();
+    this.myAnchor = document.querySelector('anchor_' + this.fragment);
   }
 
-  fragment!:any;
-  myAnchor!:any;
-  private _positions : any[] = [];
-  @ViewChildren("anchor_") divs!:QueryList<ElementRef>;
-  @Output() selectButton : EventEmitter<number> = new EventEmitter();
 
-  scrollTo($event:string){
-    const row:any = this.divs.find((e:any) => "anchor_"+$event === e.el.id);
-    console.log("row : ",row);
-    
-    // console.log("scrolling to row = ",row);
-    this.ionContent.scrollToPoint(0,row.el.offsetTop,250);
+  public scrollTo($event:string){
+    const row:any = this.divs.find((e:any) => "anchor_"+$event === e.nativeElement.id);
+    this.ionContent.scrollToPoint(0,row.nativeElement.offsetTop,250);
   }
 
   onScroll(event:any) {
-    console.log("scrolling ",event.detail.scrollTop);
-    
-    this.initElemPositions();
     let actualPosition = event.detail.scrollTop;
-    // this._positions.map((e:any)=> console.log(e.el.offsetTop));
 
     let elementToSelectIndex = 0;
     for(let i = 0;i<this._positions.length;i++){
@@ -94,36 +54,17 @@ export class AboutpageComponent {
         break;
       }
     }
-    // TODO
+    // TODO Color tabs according to position on AboutPage
     // this.navigationComp.setButtonActive(elementToSelectIndex);
     this.selectButton.emit(elementToSelectIndex);
 
   }
 
-
-  // scrollToElement(element:any): void {
-  //   console.log("scrollToElement : ",element);
-    
-  //   element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-  // }
-
-
   initElemPositions(){
     if(this._positions.length > 0)
       return;
 
-    console.log("divs : ",this.divs);
-    
-    console.log("sibling : ",(this.divs as any).nextSibling);
-    
-
-    this._positions = this.divs.filter((e:any) => {
-      console.log("e : ",e);
-      
-      return e.nativeElement.id.includes("anchor_")
-    })
-    console.log("positions : ",this._positions);
-    
+    this._positions = this.divs.filter((e:any) => e.nativeElement.id.includes("anchor_"))
   }
 
 }
--- src/app/features/coursesmanager/components/navbar/navbar.component.ts
+++ src/app/features/coursesmanager/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild, ViewChildren } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { UsermanagementService } from 'src/app/shared/service/usermanagement.service';
+// import {App} from ‘@ionic-angular’;
 
 @Component({
   selector: 'app-navbar',
@@ -8,28 +9,21 @@ import { UsermanagementService } from 'src/app/shared/service/usermanagement.ser
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  constructor(private readonly _userS:UsermanagementService, private readonly _router:Router){
-    _router.events.subscribe(s => {
-      if (s instanceof NavigationEnd) {
-        const tree = _router.parseUrl(_router.url);
-        if (tree.fragment) {
-          const element = document.querySelector("#" + tree.fragment);
-          if (element) { element.scrollIntoView(true); }
-        }
-      }
-    });
-  }
+  // @Output() itemChosen : EventEmitter<number> = new EventEmitter();
+  @ViewChildren('aboutIonContent') content!: ElementRef<HTMLInputElement>;
+
+  constructor(private readonly _userS:UsermanagementService, private readonly _router:Router){ }
 
   isAdmin = this._userS.isLoggedAsAdmin;
   isTeacher = this._userS.isLoggedAsTeacher;
   isLogged = this._userS.isLogged;
 
   navigationItems = [
-    {title:"About us",routerLink:"/about",fragment:"about"},
-    {title:"Activities",routerLink:"/about",fragment:"activities"},
-    {title:"Partners",routerLink:"/about",fragment:"partners"},
-    {title:"Contact",routerLink:"/about",fragment:"contact"},
-    {title:"Donate",routerLink:"/about",fragment:"donate"},
+    {title:"About us",page:"about",routerLink:"/about",fragment:"about"},
+    {title:"Activities",page:"about",routerLink:"/about",fragment:"activities"},
+    {title:"Partners",page:"about",routerLink:"/about",fragment:"partners"},
+    {title:"Contact",page:"about",routerLink:"/about",fragment:"contact"},
+    {title:"Donate",page:"about",routerLink:"/about",fragment:"donate"},
   ]
 
   simpleNavItems = [
@@ -37,15 +31,13 @@ export class NavbarComponent {
     {title:"Schedule",page:"schedule"},
   ]
 
-  goToAnchor(routerLink:string|undefined,fragment?:string|undefined){
-    console.log("current url : ",this._router.url);
-    
-    if(!fragment){
-      console.log("Going to link ",routerLink);
-      this._router.navigate( [routerLink]);
-    }else{
-      console.log("Going to link ",routerLink," and anchor ",fragment);
-      this._router.navigate( [routerLink], {fragment: fragment});
+  goToAnchor(link : {title:string,routerLink:string,fragment:string}){
+    this._router.navigate([link.routerLink],{fragment:link.fragment});
+ 
+    if(this._router.url.includes("about")){
+      let aboutDoc = document.getElementById("anchor_"+link.fragment);
+
+      aboutDoc?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
   }
 }
