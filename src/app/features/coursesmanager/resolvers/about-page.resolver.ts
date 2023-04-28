e7d9a72eadca5aabb363e3010e845f131b9ead1a
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-04-28 13:25:59 +0200

    about : observable activities

--- src/app/features/coursesmanager/resolvers/about-page.resolver.ts
+++ src/app/features/coursesmanager/resolvers/about-page.resolver.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { DocumentData } from 'firebase/firestore';
-import { firstValueFrom } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
 import { AngularfireService } from 'src/app/shared/service/angularfire.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AboutPageResolver implements Resolve<{members:DocumentData[],partners:DocumentData[],activities:DocumentData[]}> {
+export class AboutPageResolver implements Resolve<{members:DocumentData[],partners:DocumentData[],activities:Observable<DocumentData[]>}> {
   constructor(private readonly _db:AngularfireService){}
 
-  async resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<{members:DocumentData[],partners:DocumentData[],activities:DocumentData[]}> {
+  async resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<{members:DocumentData[],partners:DocumentData[],activities:Observable<DocumentData[]>}> {
 
     const result = {
       members : await firstValueFrom(this._db.getAssoMembers()),
       partners : await firstValueFrom(this._db.getPartners()),
-      activities : await this._db.getActivities(),
+      activities : await this._db.getActivitiesObs(),
     }
 
     return result;
