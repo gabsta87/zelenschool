f6ea6142c1362ae1ee0615b2c1fcd36b851ad855
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-05-19 21:17:08 +0200

    student modal : children display

--- src/app/features/coursesmanager/components/schedule/student-modal/student-modal.component.ts
+++ src/app/features/coursesmanager/components/schedule/student-modal/student-modal.component.ts
@@ -2,8 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import dayjs from 'dayjs';
 import { DocumentData } from 'firebase/firestore';
-import { firstValueFrom, Observable } from 'rxjs';
-import { AngularfireService } from 'src/app/shared/service/angularfire.service';
+import { BehaviorSubject, firstValueFrom, Observable } from 'rxjs';
+import { AngularfireService, UserInfos } from 'src/app/shared/service/angularfire.service';
 import { getNowDate } from 'src/app/shared/service/hour-management.service';
 import { LanguageManagerService } from 'src/app/shared/service/language-manager.service';
 import { UsermanagementService } from 'src/app/shared/service/usermanagement.service';
@@ -15,58 +15,82 @@ import { UsermanagementService } from 'src/app/shared/service/usermanagement.ser
   styleUrls: ['./student-modal.component.scss']
 })
 export class StudentModalComponent {
-  @ViewChild('popover') popover : any;
-  meta!:any;
-  dataObs!:Observable<DocumentData|undefined>;
+  @ViewChild('popover') popover: any;
+  meta!: any;
+  calendarEntries$!: Observable<DocumentData | undefined>;
 
   isPopoverOpen = false;
 
-  title!:string;
-  creator!:DocumentData|undefined;
+  title!: string;
+  creator!: DocumentData | undefined;
 
-  isAttending!:boolean;
-  isCourseFull!:boolean;
-  isSubscribtionBlocked!:boolean;
+  isParent !: BehaviorSubject<boolean>;
+  isAttending!: boolean;
+  isChildAttending : boolean[] = [];
+  isCourseFull!: boolean;
+  isSubscribtionBlocked!: boolean;
   ban = this._user.getUserData() ? this._user.getUserData().ban : null;
+  currentUser = this._user.getUserData();
+  // TODO load names
   showBanInfo = false;
 
+  courseActualValues !:any; 
+
   duration = 1;
-  durationUnit!:dayjs.ManipulateType;
+  durationUnit!: dayjs.ManipulateType;
 
   words$ = this._lang.currentLanguage$;
 
   constructor(private modalCtrl: ModalController,
     private readonly _db: AngularfireService,
-    private readonly _user:UsermanagementService,
-    private readonly _lang:LanguageManagerService
-    ) { }
-  
-  async ionViewWillEnter(){
-    this.dataObs = this._db.getCalendarEntry(this.meta.id);
+    private readonly _user: UsermanagementService,
+    private readonly _lang: LanguageManagerService
+  ) { }
 
-    let courseActualValues = await firstValueFrom(this.dataObs);
+  async ionViewWillEnter() {
+    
+    // Loading children infos
+    if(this.currentUser.children && this.currentUser.children.length > 0){
+      
+      this.currentUser.childrenInfos = await Promise.all(this.currentUser.children.map((childId: string) => this._db.getUser(childId)));
+    }
+    
+    this.calendarEntries$ = this._db.getCalendarEntry(this.meta.id);
 
-    if(courseActualValues){
+    let courseActualValues = await firstValueFrom(this.calendarEntries$);
+    this.courseActualValues = courseActualValues;
+
+    if (courseActualValues) {
 
       this.creator = await this._db.getUser(courseActualValues['author']);
 
       this.isAttending = courseActualValues['attendantsId'].includes(this._user.getId());
       this.isCourseFull = courseActualValues['attendantsId'].length >= courseActualValues['max_participants'];
 
-      this.duration = dayjs(courseActualValues['timeEnd']).diff(courseActualValues['timeStart'],"minute");
-      if(this.duration < 60 || this.duration % 60 != 0){
+      this.isParent = this._user.isParent;
+
+      if(this.currentUser.children && this.currentUser.children.length > 0){
+        this.currentUser.children.forEach((child:any) => {
+          this.isChildAttending.push(this.courseActualValues['attendantsId'].includes(child['id']))
+        })
+      }
+      console.log("isChildAttending : ",this.isChildAttending);
+      
+
+      this.duration = dayjs(courseActualValues['timeEnd']).diff(courseActualValues['timeStart'], "minute");
+      if (this.duration < 60 || this.duration % 60 != 0) {
         this.durationUnit = "minute"
-      }else{
-        this.duration = dayjs(courseActualValues['timeEnd']).diff(courseActualValues['timeStart'],"hour");
+      } else {
+        this.duration = dayjs(courseActualValues['timeEnd']).diff(courseActualValues['timeStart'], "hour");
         this.durationUnit = "hour"
       }
 
-      this.isSubscribtionBlocked = 
+      this.isSubscribtionBlocked =
         // The course is full
-        (this.isCourseFull && !this.isAttending) || 
-        
+        (this.isCourseFull && !this.isAttending) ||
+
         // User is not logged
-        !this._user.isLogged.value || 
+        !this._user.isLogged.value ||
 
         // User is banned
         this._user.isBanned() ||
@@ -75,20 +99,20 @@ export class StudentModalComponent {
         dayjs(courseActualValues['timeStart']).isBefore(getNowDate()) ||
 
         // Subscribed and 12 hours before course
-        (this.isAttending && dayjs(courseActualValues['timeStart']).subtract(12,"hour").isBefore(getNowDate())) ||
+        (this.isAttending && dayjs(courseActualValues['timeStart']).subtract(12, "hour").isBefore(getNowDate())) ||
 
         // Still possible to subscribe 12 hours after the course
-        (!this.isAttending && dayjs(courseActualValues['timeStart']).add(12,"hour").isBefore(getNowDate()))
+        (!this.isAttending && dayjs(courseActualValues['timeStart']).add(12, "hour").isBefore(getNowDate()))
     }
   }
 
-  ionViewDidEnter(){
-    if(this.ban){
+  ionViewDidEnter() {
+    if (this.ban) {
       this.showBanInfo = true;
     }
   }
 
-  cancel(){
+  cancel() {
     return this.modalCtrl.dismiss(null, 'confirm');
   }
 
@@ -96,21 +120,23 @@ export class StudentModalComponent {
     this.isPopoverOpen = true;
   }
 
-  confirm(){
-    if(!this.isSubscribtionBlocked){
-      this._db.toggleSubscribtionToCalendarEntry(this.meta.id, this.isAttending);
-      if(this.isAttending){
+  async confirm() {
+    if (!this.isSubscribtionBlocked) {
+      this._db.toggleSubscribtionToCalendarEntryForCurrentUser(this.meta.id, this.isAttending);
+      if (this.isAttending) {
         this.presentPopover();
       }
     }
 
-    // if(!this._user.isBanned()){
-    //   if(!(this.isAttending && dayjs(this.meta.timeStart).subtract(12,"hour").isBefore(getNowDate()))){
-    //     this._db.toggleSubscribtionToCalendarEntry(this.meta.id, this.isAttending);
-    //     if(this.isAttending)
-    //       this.presentPopover();
-    //   }
-    // }
+    // Children management
+    if(this.currentUser.childrenInfos){
+      console.log("subscribe");
+      
+      this.currentUser.childrenInfos.forEach((child:any,index:number) => {
+        this._db.toggleSubscribtionToCalendarEntry(child['id'],this.meta.id,this.isChildAttending[index])
+      })
+    }
+
     return this.modalCtrl.dismiss(null, 'confirm');
   }
 }
