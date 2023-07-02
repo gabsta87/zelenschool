b4e05b68302e9592bbbb525e1adef3b4b4807659
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2023-07-02 12:28:54 +0200

    dates inputs corrected

--- src/app/features/coursesmanager/components/account/accountpage/accountpage.component.ts
+++ src/app/features/coursesmanager/components/account/accountpage/accountpage.component.ts
@@ -85,4 +85,25 @@ export class AccountpageComponent {
     }
   }
 
+  onInput(ev:any) {
+    const value = ev.target!.value;
+    
+    // Removes non alphanumeric characters
+    const numeric = value.replace(/[^\d./]/, "");
+
+    // Inserts dots
+    const correctedValue = numeric.replace(/(?<=^\d{2})(\d)/g, ".$1");
+    const correctedSecondLevel = correctedValue.replace(/(?<=^\d{2}\.\d{2})(\d)/g, ".$1");
+
+    // Blocks extra characters
+    const truncatedValue = correctedSecondLevel.replace(/(?<=(?:^\d{2}[./]\d{2}[./]\d{4}))./g,"")
+
+    /**
+     * Update both the state variable and
+     * the component to keep them in sync.
+     */
+    ev.target.value = truncatedValue;
+    this.profileForm.get("birthday")?.setValue(truncatedValue);
+  }
+
 }
--- src/app/features/coursesmanager/components/account/child-creation-modal/child-creation-modal.component.ts
+++ src/app/features/coursesmanager/components/account/child-creation-modal/child-creation-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { LanguageManagerService } from 'src/app/shared/service/language-manager.service';
+import { bdValidator } from 'src/app/shared/service/validators-lib.service';
 
 @Component({
   selector: 'app-child-creation-modal',
@@ -8,6 +10,13 @@ import { LanguageManagerService } from 'src/app/shared/service/language-manager.
   styleUrls: ['./child-creation-modal.component.scss']
 })
 export class ChildCreationModalComponent {
+
+  profileForm!:FormGroup<{
+    birthday:FormControl<string|null>,
+    l_name:FormControl<string|null>,
+    f_name:FormControl<string|null>,
+  }>;
+
   child: {
     f_name: string,
     l_name: string,
@@ -20,13 +29,50 @@ export class ChildCreationModalComponent {
 
   words$ = this._lang.currentLanguage$;
 
-  constructor(private readonly modalCtrl: ModalController, private readonly _lang:LanguageManagerService) { }
+  constructor(private readonly modalCtrl: ModalController, private readonly _lang:LanguageManagerService) {
+    this.profileForm = new FormGroup({
+
+      birthday: new FormControl('',Validators.compose([
+        bdValidator(),
+        Validators.required
+      ])),
+
+      l_name: new FormControl('',Validators.required),
+      f_name: new FormControl('',Validators.required),
+    });
+  }
 
   cancel() {
     this.modalCtrl.dismiss(null,"cancel");
   }
 
   confirm(){
-    return this.modalCtrl.dismiss(this.child, "confirm");
+    let returnChild = {
+      f_name:this.profileForm.get("f_name"),
+      l_name:this.profileForm.get("l_name"),
+      birthday:this.profileForm.get("birthday")
+    }
+    return this.modalCtrl.dismiss(returnChild, "confirm");
+  }
+
+  onInput(ev:any) {
+    const value = ev.target!.value;
+    
+    // Removes non alphanumeric characters
+    const numeric = value.replace(/[^\d./]/, "");
+
+    // Inserts dots
+    const correctedValue = numeric.replace(/(?<=^\d{2})(\d)/g, ".$1");
+    const correctedSecondLevel = correctedValue.replace(/(?<=^\d{2}\.\d{2})(\d)/g, ".$1");
+
+    // Blocks extra characters
+    const truncatedValue = correctedSecondLevel.replace(/(?<=(?:^\d{2}[./]\d{2}[./]\d{4}))./g,"")
+
+    /**
+     * Update both the state variable and
+     * the component to keep them in sync.
+     */
+    ev.target.value = truncatedValue;
+    this.profileForm.get("birthday")?.setValue(truncatedValue);
   }
 }
