694e629786eb194faa6274ea093e6e8f35c74f19
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2024-03-17 16:42:15 +0100

    event modal : participants not erased anymore

--- src/app/features/coursesmanager/components/admin/event-modal/event-modal.component.ts
+++ src/app/features/coursesmanager/components/admin/event-modal/event-modal.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { DocumentData } from 'firebase/firestore';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { AngularfireService } from 'src/app/shared/service/angularfire.service';
 import { StorageService } from 'src/app/shared/service/storage.service';
 
@@ -16,7 +16,6 @@ export class EventModalComponent {
   profileForm!:FormGroup<{
     name:FormControl<string|null>,
     location:FormControl<string|null>,
-    participants:FormControl<string|null>,
     leafletLink:FormControl<string|null>,
     startDay:FormControl<string|null>,
     endDay:FormControl<string|null>,
@@ -28,11 +27,12 @@ export class EventModalComponent {
   leafletLink!: string;
   location!: string;
   name!: string;
-  participants!: string;
+  participants!: string[];
   timeStart!: string;
   timeEnd!: string;
 
   galleries!: Observable<DocumentData[]>;
+  users!: Observable<DocumentData[]>;
 
   constructor(
     private readonly modalCtrl: ModalController,
@@ -40,33 +40,35 @@ export class EventModalComponent {
     private readonly _storage : StorageService,
   ) {
     this.galleries = this._db.getGalleries();
+    this.users = this._db.getUsers().pipe(map((users:DocumentData[]) => users.filter((user:DocumentData) => this.participants.includes(user['id']))))
   }
 
   ngOnInit(){
     this.profileForm = new FormGroup({
       name: new FormControl(this.name, Validators.required),
       location: new FormControl(this.location),
-      participants: new FormControl(this.participants),
       leafletLink: new FormControl(this.leafletLink),
       startDay: new FormControl(this.timeStart),
       endDay: new FormControl(this.timeEnd),
       galleryId: new FormControl(this.galleryId),
     });
+    if(!this.participants)
+      this.participants = [];
   }
 
   confirm() {
     if(!this.profileForm.valid)
-    // if(!this.dayInputValid || !this.profileForm.valid)
       return;
+
     let entry = {
       id: this.id,
       name: this.profileForm.get('name')?.value,
       location: this.profileForm.get('location')?.value,
-      participants: this.profileForm.get('participants')?.value,
       leafletLink: this.profileForm.get('leafletLink')?.value,
-      timeStart: this.profileForm.get('timeStart')?.value,
-      timeEnd: this.profileForm.get('timeEnd')?.value,
+      timeStart: this.profileForm.get('startDay')?.value,
+      timeEnd: this.profileForm.get('endDay')?.value,
       galleryId: this.profileForm.get('galleryId')?.value,
+      participants : this.participants
     }
 
     return this.modalCtrl.dismiss(entry, 'confirm');
@@ -79,9 +81,5 @@ export class EventModalComponent {
   get dayInputValid(): boolean {
     return (this.profileForm.get('startDay')?.valid && this.profileForm.get('endDay')?.valid) || false;
   }
-
-  // selectGallery(id:string){
-  //   this.galleryId = id;
-  // }
 
 }
