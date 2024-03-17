6560db430e8322a39fee1b5c8ee8631de2384d29
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2024-03-17 15:16:09 +0100

    asso event : popover dismiss on gallery view

--- src/app/features/coursesmanager/components/presentation/asso-event-popover/asso-event-popover.component.ts
+++ src/app/features/coursesmanager/components/presentation/asso-event-popover/asso-event-popover.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
 import { DocumentData } from 'firebase/firestore';
 
 @Component({
@@ -10,9 +11,12 @@ import { DocumentData } from 'firebase/firestore';
 export class AssoEventPopoverComponent {
   assoEvent!:DocumentData;
 
-  constructor(private readonly _router : Router){ }
+  constructor(private readonly _router : Router,
+    private readonly _popoverControler : PopoverController
+    ){ }
 
   goToGallery(galleryId:string){
+    this._popoverControler.dismiss();
     this._router.navigate(["gallery"],{fragment:galleryId});
   }
 
